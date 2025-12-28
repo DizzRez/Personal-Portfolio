e7ac735bcaeac47753e02d621cc98268aebd0b4c
DizzRez <dizzrez@gmail.com>
2025-12-28 11:07:43 +0700

    adding effects

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const dropdownMenu = document.getElementById("dropdownMenu");
 burger.addEventListener("click", () => {
   dropdownMenu.classList.toggle("active");
 });
+
+let aboutmepic= document.querySelector('.imgcirc')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 50 * dx / window.innerWidth / 2
+    let angleY = 50 * dy / window.innerHeight / 2
+    aboutmepic.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
+
+let circles= document.querySelector('.circ-2')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 50 * dx / window.innerWidth / 2
+    let angleY = 50 * dy / window.innerHeight / 2
+    circles.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
